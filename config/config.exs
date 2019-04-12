650c0e3530857aec2f629be4896084c1876c33f9
Jacqui Manzi <jacquimanzi@gmail.com>
2019-04-11 21:19:08 -0700

    Separate configuration for the rpi3

--- config/config.exs
+++ config/config.exs
@@ -15,7 +15,7 @@ config :nerves, :firmware, rootfs_overlay: "rootfs_overlay"
 # involved with firmware updates.
 
 config :shoehorn,
-  init: [:nerves_runtime, :nerves_init_gadget],
+  init: [:nerves_runtime],
   app: Mix.Project.config()[:app]
 
 # Use Ringlogger as the logger backend and remove :console.
@@ -44,22 +44,12 @@ if keys == [],
     See your project's config.exs for this error message.
     """)
 
-config :nerves_firmware_ssh,
-  authorized_keys: Enum.map(keys, &File.read!/1)
-
 # Configure nerves_init_gadget.
 # See https://hexdocs.pm/nerves_init_gadget/readme.html for more information.
 
 # Setting the node_name will enable Erlang Distribution.
 # Only enable this for prod if you understand the risks.
 node_name = if Mix.env() != :prod, do: "night_vision"
-
-config :nerves_init_gadget,
-  mdns_domain: "nerves.local",
-  node_name: node_name,
-  node_host: :mdns_domain,
-  ifname: "eth0",
-  address_method: :dhcp
 
 # Import target specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
