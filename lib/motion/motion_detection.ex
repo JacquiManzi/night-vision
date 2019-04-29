421d4aeae91fbcd47b1f5e2c5e3e99ca4d9c382d
Jacqui Manzi <jacquimanzi@gmail.com>
2019-04-28 17:34:51 -0700

    Section creation

--- lib/motion/motion_detection.ex
+++ lib/motion/motion_detection.ex
@@ -6,9 +6,9 @@ defmodule NightVision.Motion.MotionDetection do
     decompressed_image = File.read!("testy.bmp")
 
     # @TODO: Remove this dependancy and just use the hex values for the width and height
-    {_format, width, height, _encode} = ExImageInfo.info(decompressed_image)
+    {_format, _width, height, _encode} = ExImageInfo.info(decompressed_image)
     pixel_list = get_pixel_list(decompressed_image)
-    get_image_sections(pixel_list, width, height)
+    get_image_sections(pixel_list, height)
   end
 
   defp get_pixel_list(image_binary) do
@@ -16,19 +16,30 @@ defmodule NightVision.Motion.MotionDetection do
     |> :binary.bin_to_list()
   end
 
-  defp get_image_sections(pixel_list, width, height) do
+  defp get_image_sections(pixel_list, height) do
     row_length = (length(pixel_list) / height) |> round()
 
-    rows =
-      pixel_list
-      |> Enum.chunk_every(row_length)
-
-    File.write!("./row.bmp", concat_rows(rows, [], 0), [])
+    [top_left, top_right, bottom_left, bottom_right] =
+      pixel_list |> Enum.chunk_every(row_length) |> create_sections()
   end
 
-  defp concat_rows(rows, new_rows, 637), do: new_rows
+  defp create_sections(image_rows) do
+    [top, bottom] = split_list(image_rows)
+    split_list(top, [], []) ++ split_list(bottom, [], [])
+  end
 
-  defp concat_rows(rows, new_rows, count) do
-    concat_rows(rows, new_rows ++ Enum.at(rows, count), count + 1)
+  defp split_list([], left, right), do: [left, right]
+
+  defp split_list(list, left, right) do
+    {val, remaining} = List.pop_at(list, 0)
+    [new_left, new_right] = split_list(val)
+    split_list(remaining, left ++ new_left, right ++ new_right)
+  end
+
+  defp split_list(nil), do: [[], []]
+
+  defp split_list(list) do
+    half_length = (length(list) / 2) |> round()
+    Enum.chunk_every(list, half_length)
   end
 end
